8e08eda1266073702d8feea61ea0d0173a72c5d9
pixieez <160623847+pixieez@users.noreply.github.com>
2024-02-25 03:08:51 +0700

    Update predictor.py

--- roop/predictor.py
+++ roop/predictor.py
@@ -1,6 +1,5 @@
 import threading
 import numpy
-import opennsfw2
 from PIL import Image
 from keras import Model
 
@@ -10,12 +9,14 @@ PREDICTOR = None
 THREAD_LOCK = threading.Lock()
 MAX_PROBABILITY = 0.85
 
+USE_OPENNSFW2 = False
 
 def get_predictor() -> Model:
     global PREDICTOR
 
     with THREAD_LOCK:
-        if PREDICTOR is None:
+        if PREDICTOR is None and USE_OPENNSFW2:
+            import opennsfw2
             PREDICTOR = opennsfw2.make_open_nsfw_model()
     return PREDICTOR
 
@@ -27,6 +28,10 @@ def clear_predictor() -> None:
 
 
 def predict_frame(target_frame: Frame) -> bool:
+    if not USE_OPENNSFW2:
+        return False
+
+    import opennsfw2
     image = Image.fromarray(target_frame)
     image = opennsfw2.preprocess_image(image, opennsfw2.Preprocessing.YAHOO)
     views = numpy.expand_dims(image, axis=0)
@@ -35,9 +40,17 @@ def predict_frame(target_frame: Frame) -> bool:
 
 
 def predict_image(target_path: str) -> bool:
+    if not USE_OPENNSFW2:
+        return False
+
+    import opennsfw2
     return opennsfw2.predict_image(target_path) > MAX_PROBABILITY
 
 
 def predict_video(target_path: str) -> bool:
+    if not USE_OPENNSFW2:
+        return False
+
+    import opennsfw2
     _, probabilities = opennsfw2.predict_video_frames(video_path=target_path, frame_interval=100)
     return any(probability > MAX_PROBABILITY for probability in probabilities)
